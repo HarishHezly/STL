897b864e0f8e0a4e17e25106b270a47b4ebf12f2
HarishHezly <harishhezly12@gmail.com>
2018-04-14 08:53:57 +0800

    Made a dynamic table to fill in data

--- vectorexe2.cpp
+++ vectorexe2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -31,8 +33,42 @@ class Table
         return 0;
     }
 
-    void printTable(){
-        
+    int get_max_length()
+    {
+        vector<string>::iterator it;
+        int max = 0;
+        for (it = name.begin(); it != name.end(); it++)
+        {
+            string tmp = *it;
+            if (tmp.length() > max)
+            {
+                max = tmp.length();
+            }
+        }
+        return max;
+    }
+    void printTable()
+    {
+        int length = get_max_length();
+        if (length < 8)
+        {
+            length = 8;
+        }
+
+        cout << "|" << setw(length) << "Name" << setw(0) << "|" 
+                    << setw(5)      << "Age"  << setw(0) << "|" << endl;
+
+        cout << "+" << setfill('-') << setw(length)<< "-"  << setfill(' ') <<setw(0) << "+"
+                    << setfill('-') << setw(5)     << "-"  << setfill(' ') <<setw(0) << "+" << endl;
+
+        for (int i = 0; i < name.size(); i++)
+        {
+            cout << "|" << setw(length) << name[i] << setw(0) << "|"
+                        <<  setw(5)     << age[i]  << setw(0) << "|" << endl; 
+        }
+
+        cout << "+"<< setfill('-') << setw(length)<<"-" << setw(0) << "+"
+                   << setfill('-') << setw(5)<<"-" << setw(0) << "+" << endl;
     }
 
   private:
@@ -45,16 +81,29 @@ int main()
     Table mytable;
     int a = 0;
     string n;
-
-    while (a != -1)
+    char t = 'y';
+    bool cont = true;
+    while (cont)
     {
         cout << "Enter your name = ";
         cin >> n;
         cout << "Enter your age = ";
         cin >> a;
+        cout << "Continue adding (y/n):";
+        cin >> t;
+        cin.ignore();
 
+        if (t == 'y')
+        {
+            cont = true;
+        }
+
+        else
+        {
+            cont = false;
+        }
         mytable.add_data(n, a);
     }
-
+    
     mytable.printTable();
 }
