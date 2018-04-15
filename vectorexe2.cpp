e3c53c79dc49b2ee722254ce77f86a08730be8be
HarishHezly <harishhezly12@gmail.com>
2018-04-15 09:25:31 +0800

    Add find function and change methods

--- vectorexe2.cpp
+++ vectorexe2.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <list>
 #include <string>
 #include <iomanip>
+#include <iterator>
 
 using namespace std;
 
 class Table
 {
   public:
+    
+    void change_name(string old_name, string new_name, int new_age)
+    {
+        itName = find (name.begin(), name.end(), old_name);
+
+        if (itName == name.end())
+        {
+            cout << "Name is not in table" << endl;
+        }
+
+        else
+        {
+            cout << "Changing old name to new name" << endl;
+            *itName = new_name;
+            int d = distance(name.begin(), itName);
+            itAge = age.begin() + d;
+            *itAge = new_age;
+        }
+    }
+
     void add_data(string n, int a)
     {
         name.push_back(n);
@@ -17,7 +40,7 @@ class Table
     string get_name(int index)
     {
         if (index < name.size())
-            return name[index];
+            return "";//name[index];
         else
             throw "error";
         return "";
@@ -27,7 +50,7 @@ class Table
     {
 
         if (index < age.size())
-            return age[index];
+            return 0;//age[index];
         else
             throw 0;
         return 0;
@@ -35,11 +58,11 @@ class Table
 
     int get_max_length()
     {
-        vector<string>::iterator it;
+
         int max = 0;
-        for (it = name.begin(); it != name.end(); it++)
+        for (itName = name.begin(); itName != name.end(); itName++)
         {
-            string tmp = *it;
+            string tmp = *itName;
             if (tmp.length() > max)
             {
                 max = tmp.length();
@@ -55,16 +78,21 @@ class Table
             length = 8;
         }
 
+        cout << "\n+" << setfill('-') << setw(length)<< "-"  << setfill(' ') <<setw(0) << "+"
+                    << setfill('-') << setw(5)     << "-"  << setfill(' ') <<setw(0) << "+" << endl;
+
         cout << "|" << setw(length) << "Name" << setw(0) << "|" 
                     << setw(5)      << "Age"  << setw(0) << "|" << endl;
 
         cout << "+" << setfill('-') << setw(length)<< "-"  << setfill(' ') <<setw(0) << "+"
                     << setfill('-') << setw(5)     << "-"  << setfill(' ') <<setw(0) << "+" << endl;
 
-        for (int i = 0; i < name.size(); i++)
+
+        
+        for (itName = name.begin(),itAge = age.begin() ; itName != name.end(); itName++, itAge++)
         {
-            cout << "|" << setw(length) << name[i] << setw(0) << "|"
-                        <<  setw(5)     << age[i]  << setw(0) << "|" << endl; 
+            cout << "|" << setw(length) << *itName << setw(0) << "|"
+                        <<  setw(5)     << *itAge  << setw(0) << "|" << endl; 
         }
 
         cout << "+"<< setfill('-') << setw(length)<<"-" << setw(0) << "+"
@@ -74,6 +102,8 @@ class Table
   private:
     vector<string> name;
     vector<int> age;
+    vector<string>::iterator itName;
+    vector<int>::iterator itAge;
 };
 
 int main()
@@ -106,4 +136,22 @@ int main()
     }
     
     mytable.printTable();
+
+    cout << "Enter the name you want to change : ";
+    string old_name;
+    cin >> old_name;
+
+    cout << "Enter new name to change "<< old_name <<": ";
+    string new_name;
+    cin >> new_name;
+
+    cout << "Enter new age for"<< new_name <<": ";
+    int new_age;
+    cin >> new_age;
+
+    mytable.change_name(old_name, new_name, new_age);
+    
+
+    mytable.printTable();
+
 }
